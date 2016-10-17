75d7f6a75e6d724da25a7940114ef494fa02790e
Joao Coelho <joaocoelho93@gmail.com>
2016-10-17 17:23:11 +0000

    Adds conditional exercises

---
+++ ageCalculator.js
@@ -0,0 +1,12 @@
+
+function ageCalculator(name, yearOfBirth, currentYear){
+
+  console.log (name + " is " + (currentYear - yearOfBirth) + " years old.");
+}
+
+
+//console.log(ageCalculator("Miranda", 1983, 2015));
+//console.log(ageCalculator("Ferdinand", 1988, 2015));
+ageCalculator('Suzie', 1983, 2015);
+ageCalculator("Jack", 2003, 2015);
+ageCalculator("Ali", 2015, 2015);
